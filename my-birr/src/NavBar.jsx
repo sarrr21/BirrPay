66715d5f32c37cc36555d56fbb16b7e8dc7d5206
Naol Ketema <naolketema55@gmail.com>
2024-04-26 19:00:34 +0300

    some unsesery code removed

--- my-birr/src/NavBar.jsx
+++ my-birr/src/NavBar.jsx
@@ -30,7 +30,7 @@ const Navbar = ({ onLogout }) => {
           Logout
         </Link>
       </div> */}
-      <div className="navbar bg-base-100">
+      <div className="navbar ">
         <div className="navbar-start">
           <div className="dropdown">
             <div
